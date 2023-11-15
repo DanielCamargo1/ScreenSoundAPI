6f860ba4e313a53f670f9ad2b4a69e4d31e06324
Daniel Camargo <danidcp098@gmail.com>
2023-11-15 12:25:03 -0300

    Adicionando a api do chat gpt

---
+++ ConsoleScreenSoundAPI/Filtros/LinqFiltros.cs
@@ -0,0 +1,91 @@
+﻿using ConsoleScreenSoundAPI.Modelos;
+using OpenAI_API;
+using System;
+using System.Linq;
+namespace ConsoleScreenSoundAPI.Filtros;
+
+internal class LinqFiltros
+{
+    public static void FilterTodosOsGenerosMusicais(List<Musica> musicas)
+    {
+        var TodosOsGenerosMusicais = musicas.Select(generos => generos.Genero).Distinct().ToList();
+        foreach(var genero in TodosOsGenerosMusicais)
+        {
+            Console.WriteLine($"- {genero}");
+        }
+    }
+
+    public static void FiltarArtistaPorGenero(List<Musica> musicas, string genero)
+    {
+        var artistasPorGenero = musicas.Where(musica => musica.Genero.Contains(genero)).Select(musica => musica.Artista).Distinct().ToList();
+        Console.WriteLine($"Exibindo artista pelo genero: {genero}");
+        if(artistasPorGenero.Count > 0)
+        {
+            foreach(var artista in artistasPorGenero)
+            {
+                  Console.WriteLine($"Artista : {artista}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nada encontrado!");
+        }
+         Console.WriteLine($"Ao todo são {artistasPorGenero.Count} artistas registrados para {genero} ");
+    }
+
+    public static void FiltrarMusicasDeUmArtista(List<Musica> musicas, string nomeArtista)
+    {
+        var musicaDeUmArtista = musicas.Where(musica => musica.Artista.Contains(nomeArtista)).Distinct().ToList();
+        if(musicaDeUmArtista.Count > 0)
+        {
+            Console.WriteLine($"Exibindo musicas de {nomeArtista}");
+            foreach(var musica in musicaDeUmArtista)
+            {
+            
+                Console.WriteLine($"Musica : {musica.Nome}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Não encontrado musicas para: {nomeArtista}");
+        }
+    }
+
+    public static void FiltrarMusicasPorAno(List<Musica> musicas, int ano)
+    {
+        var musicasPorAno = musicas.Where(musica => musica.Ano == ano).OrderBy(musicas => musicas.Nome).Distinct().ToList();
+        Console.WriteLine($"Exibindo musicas de {ano}");
+        if(musicasPorAno.Count > 0)
+        {
+            foreach(var musica in musicasPorAno)
+            {
+                Console.WriteLine($"Musica: {musica.Nome}");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Não encontrado musicas para: {ano}");
+        }
+    }
+
+    public static void FiltrarPorNota(List<Musica> musicas)
+    {
+        var musicaPorNota = musicas.Where(musica => musica.Key == 1).OrderBy(musicas => musicas.Nome).ToList();
+        foreach(var musica in musicaPorNota)
+        {
+            Console.WriteLine($"Musica -{musica.Nome}:   Tocada em  {musica.tonalidade}");
+        }
+        
+    }
+
+    public static void ExibirResumodeUmaBanda(List<Musica> musicas, string banda)
+    {
+        var client = new OpenAIAPI("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");
+        var chat = client.Chat.CreateConversation();
+        chat.AppendSystemMessage($"Crie um resumo bem curto da banda {banda}");
+        string res = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        Console.WriteLine(res);
+    }
+}
+
+    
---
+++ ConsoleScreenSoundAPI/Program.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleScreenSoundAPI.Filtros;
+using ConsoleScreenSoundAPI.Modelos;
+using System.Text.Json;
+using OpenAI_API;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        //LinqFiltros.FilterTodosOsGenerosMusicais(musicas);
+        //LinqOrder.FiltrarArtistasPorNome(musicas);
+        //LinqFiltros.FiltarArtistaPorGenero(musicas, "hip hop");
+        //LinqFiltros.FiltrarMusicasDeUmArtista(musicas, "U2");
+        //LinqFiltros.FiltrarMusicasPorAno(musicas, 2020);
+
+
+        //var musicasPerferidasDoDaniel = new MusicasPreferidas("Daniel");
+        //musicasPerferidasDoDaniel.ExibirMusica();
+        //musicasPerferidasDoDaniel.GerarArquivoJson();
+
+
+        //LinqFiltros.FiltrarPorNota(musicas);
+
+
+        LinqFiltros.ExibirResumodeUmaBanda(musicas,"Coldplay");
+
+
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema : {ex.Message}");
+    }
+}
+
+
